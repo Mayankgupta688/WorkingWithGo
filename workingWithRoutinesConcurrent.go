58046428fd7ab7a491c87d4ae0571a46501b41da
Mayank.Gupta <mayank.gupta@iongroup.com>
2019-07-08 14:35:30 +0530

    Working with Parellel Programming

---
+++ workingWithRoutinesConcurrent.go
@@ -0,0 +1,18 @@
+package main
+import "time"
+import "runtime"
+
+func main() {
+	go alphabetGenerator()
+
+	// While the main function is kept to sleep, another Go Routine executes and then the function returns ..
+	
+	time.Sleep(1000 * time.Millisecond)
+	println("Go Routine Executed..")
+}
+
+func alphabetGenerator() {
+	for alpha := byte('a'); alpha <= byte('z'); alpha++ {
+		go println(string(alpha))
+	}
+}
