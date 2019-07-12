778ef6f6436a42538f76f0b2bb986354d497d533
Mayank.Gupta <mayank.gupta@iongroup.com>
2019-07-12 15:34:58 +0530

    File Reader

--- fileReaderApplicationWithGoRoutine.go
+++ fileReaderApplicationWithGoRoutine.go
@@ -9,17 +9,18 @@ import(
 const watchedPath = "./source";
 
 func main() {
-	for {
-		watcher, _ := os.Open(watchedPath)
-		files, _ := watcher.Readdir(-1)
 
-		for _, file:= range files {
+	watcher, _ := os.Open(watchedPath)
+	files, _ := watcher.Readdir(-1)
+
+	for _, file := range files {
+		func(file) {
 			filePath := watchedPath + "/" + file.Name()
 			fileOpen, _ := os.Open(filePath)
 			fileData, _ := ioutil.ReadAll(fileOpen)
 			fileOpen.Close()
 			os.Remove(filePath)
 			fmt.Println(string(fileData))
-		}
+		}(file)
 	}
 }
